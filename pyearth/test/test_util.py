3a1d669fa78b6723ea84809829ff4408d013455f
Dylan Mordaunt <15080672+edithatogo@users.noreply.github.com>
2025-07-04 23:27:51 +1200

    Replace nose with pytest

--- pyearth/test/test_util.py
+++ pyearth/test/test_util.py
@@ -9,5 +9,6 @@ class TestUtil(object):
         pass
 
 if __name__ == '__main__':
-    import nose
-    nose.run(argv=[__file__, '-s', '-v'])
+    import pytest
+    pytest.main([__file__, '-s', '-v'])
+
